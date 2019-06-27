8808fbdf2a2c078ec17764e4bbac294d6bfce9ca
Katik <katik.hello@gmail.com>
2019-06-27 22:13:20 +0600

    True Names fix py

--- 1_Kithgard_Dungeon/007-True_Names/true_names.py
+++ 1_Kithgard_Dungeon/007-True_Names/true_names.py
@@ -1,7 +1,7 @@
 """
-Optional 2 - if you can perform longrange powerfull attack uncomment 6-8 lines
-Optional 3 - if you can perform manaBlast() uncomment 8-9 lines
-Optional 2 & 3 requires to comment 11-16 lines
+Optional 1 - if you can perform longrange powerfull attack uncomment 6-8 lines
+Optional 2 - if you can perform manaBlast() uncomment 8-9 lines
+Optional 1 or 2 requires to comment 11-16 lines
 """
 # hero.attack("Brak")
 # hero.attack("Treg")
