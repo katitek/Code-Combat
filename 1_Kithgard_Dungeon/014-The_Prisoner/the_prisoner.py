2546c53716bc32145f0a36b5181fe720d28b00e8
Katik <katik.hello@gmail.com>
2019-06-29 13:27:52 +0600

    The Prisoner level

---
+++ 1_Kithgard_Dungeon/014-The_Prisoner/the_prisoner.py
@@ -0,0 +1,8 @@
+# You need the Emperor's ring to cast "Chain Lightning"
+# You need an Elemental codex 1+ yo cast "Haste"
+
+hero.cast("haste", hero)
+hero.attack("Weak Door")
+hero.moveRight(4)
+hero.moveDown(3)
+hero.cast("chain-lightning", "Two")
