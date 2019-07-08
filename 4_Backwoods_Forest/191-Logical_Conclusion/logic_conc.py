dd7df181df9eb6f105fa8a9d28af0450c647d4b1
Katik <katik.hello@gmail.com>
2019-07-08 16:10:31 +0600

    Logical Conclusion level

--- 4_Backwoods_Forest/191-Logical_Conclusion/logic_conc.py
+++ 4_Backwoods_Forest/191-Logical_Conclusion/logic_conc.py
@@ -2,12 +2,12 @@ def goTo(logic, x, y):
     hero.moveXY(x, y)
     hero.say(logic)
 
-hero.moveXY(24, 16);
+hero.moveXY(26, 16);
 a = hero.findNearestFriend().getSecretA()
 b = hero.findNearestFriend().getSecretB()
 c = hero.findNearestFriend().getSecretC()
 
-goTo(a and b or c, 19, 26)
-goTo((a or b) and c, 26, 36)
-goTo((a or c) and (b or c), 37, 34)
+goTo(a and b or c, 25, 26)
+goTo((a or b) and c, 26, 32)
+goTo((a or c) and (b or c), 35, 32)
 goTo((a and b) or (not c and b), 40, 22)
